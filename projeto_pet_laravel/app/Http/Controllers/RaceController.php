ccdb645da9ec03d99dfac60ff82375a1cbb5d0ad
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-11-28 20:49:32 -0300

    Atualização do Race Controller

--- projeto_pet_laravel/app/Http/Controllers/RaceController.php
+++ projeto_pet_laravel/app/Http/Controllers/RaceController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Race;
+use Exception;
 use Illuminate\Http\Request;
 
 class RaceController extends Controller
 {
-    //
+    public function index()
+    {
+        try {
+            $races = Race::all();
+            return $races;
+        } catch (\Throwable $th) {
+            return "Houve um erro ao listar as raças";
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            //Validações para retornar uma mensagem tratando o erro
+            $request->validate([
+                'name' => 'required|string|unique:races|max:50',
+            ]);
+
+            $data = $request->all();
+
+            $races = Race::create($data);
+            return $races;
+        } catch (Exception $error) {
+            return $this->response($error->getMessage(), null, false, 400);
+        }
+    }
 }

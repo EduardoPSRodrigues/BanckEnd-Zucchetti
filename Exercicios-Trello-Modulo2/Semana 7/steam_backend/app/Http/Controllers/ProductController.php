ce2540659c23976845d341dedd3dc4ed2ffcecbb
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-11-29 20:36:56 -0300

    Update ProductController

--- Exercicios-Trello-Modulo2/Semana 7/steam_backend/app/Http/Controllers/ProductController.php
+++ Exercicios-Trello-Modulo2/Semana 7/steam_backend/app/Http/Controllers/ProductController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function index()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+
+    }
+
 }

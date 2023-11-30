3eb17b4d1c4c54712cd3797013b074c4a18e6ab6
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-11-30 17:14:29 -0300

    Atualização das rotas

--- Exercicios-Trello-Modulo2/Semana 7/steam_backend/routes/api.php
+++ Exercicios-Trello-Modulo2/Semana 7/steam_backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\AchievementController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
-
+use App\Http\Controllers\ProductRequirementController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('products', [ProductController::class, 'index']);
@@ -16,3 +17,13 @@ Route::post('categories', [CategoryController::class, 'store']);
 Route::get('categories/{id}', [CategoryController::class, 'show']);
 Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
 Route::put('categories/{id}', [CategoryController::class, 'update']);
+
+Route::get('achievements', [AchievementController::class, 'index']);
+Route::post('achievements', [AchievementController::class, 'store']);
+Route::delete('achievements/{id}', [AchievementController::class, 'destroy']);
+Route::put('achievements/{id}', [AchievementController::class, 'update']);
+
+Route::get('products_requirements', [ProductRequirementController::class, 'index']);
+Route::post('products_requirements', [ProductRequirementController::class, 'store']);
+Route::delete('products_requirements/{id}', [ProductRequirementController::class, 'destroy']);
+Route::put('products_requirements/{id}', [ProductRequirementController::class, 'update']);

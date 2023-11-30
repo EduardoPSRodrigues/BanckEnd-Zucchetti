ee999776f1c9148a592516c71edfb1b15451f969
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-11-30 10:12:46 -0300

    Update Product

--- Exercicios-Trello-Modulo2/Semana 7/steam_backend/app/Models/Product.php
+++ Exercicios-Trello-Modulo2/Semana 7/steam_backend/app/Models/Product.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'price', 'cover', 'description'];
+
+    //Essa propriedade tem a função de converter os dados da tabela price em números
+    protected $casts = [
+        'price' => 'float'
+    ];
 }

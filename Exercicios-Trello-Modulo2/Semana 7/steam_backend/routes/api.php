50d9eeccb14b19b6f32424ffa221239d3c0ff38e
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-11-30 10:39:21 -0300

    Update Rotas da Categoria

--- Exercicios-Trello-Modulo2/Semana 7/steam_backend/routes/api.php
+++ Exercicios-Trello-Modulo2/Semana 7/steam_backend/routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('products', [ProductController::class, 'index']);
@@ -8,3 +10,9 @@ Route::post('products', [ProductController::class, 'store']);
 Route::get('products/{id}', [ProductController::class, 'show']);
 Route::delete('products/{id}', [ProductController::class, 'destroy']);
 Route::put('products/{id}', [ProductController::class, 'update']);
+
+Route::get('categories', [CategoryController::class, 'index']);
+Route::post('categories', [CategoryController::class, 'store']);
+Route::get('categories/{id}', [CategoryController::class, 'show']);
+Route::delete('categories/{id}', [CategoryController::class, 'destroy']);
+Route::put('categories/{id}', [CategoryController::class, 'update']);

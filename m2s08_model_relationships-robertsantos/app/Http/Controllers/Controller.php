12958ec94f21456a84c66a99d11150b2e35345fe
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-02 17:20:03 -0300

    Criando padronização de resposta da api
    Para response e error

--- m2s08_model_relationships-robertsantos/app/Http/Controllers/Controller.php
+++ m2s08_model_relationships-robertsantos/app/Http/Controllers/Controller.php
@@ -6,7 +6,27 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+use Symfony\Component\HttpFoundation\Response;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    //Função de resposta para todo o código
+    public function response($data, $message, $status = true, $statusCode = Response::HTTP_OK)
+    {
+        $data = [
+            "status"=> $status,
+            "message"=> $message,
+            "data"=> $data,
+        ];
+
+        return response()->json($data, $statusCode);
+    }
+
+    //Função de error para todo o código
+    public function error($message, $statusCode = Response::HTTP_INTERNAL_SERVER_ERROR)
+    {
+        return $this->response(null, $message, false, $statusCode);
+    }
 }

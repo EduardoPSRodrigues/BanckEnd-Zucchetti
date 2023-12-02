1d86e5950d8b6be0933e3245d1ee0015813e3b66
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-02 11:47:50 -0300

    Atualizando DatabaseSeeder

--- m2s08_model_relationships-robertsantos/database/seeders/DatabaseSeeder.php
+++ m2s08_model_relationships-robertsantos/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Gender as GenderModel;
+use App\Models\User as UserModel;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        if(!UserModel::count())
+        {
+            UserModel::factory(10)->create();
+        }
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        //Informar o Model que é para ele contar, se estiver vazio é para chamar (call) a classe GenderSeeder
+        if(!GenderModel::count())
+        {
+            $this->call([GenderSeeder::class]);
+        }
     }
 }

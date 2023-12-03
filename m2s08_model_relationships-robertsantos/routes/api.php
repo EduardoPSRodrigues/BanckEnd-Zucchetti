880f775fc78fc43a435297d0ee4f57ebd39cf201
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-03 18:30:28 -0300

    Atualização rotas

--- m2s08_model_relationships-robertsantos/routes/api.php
+++ m2s08_model_relationships-robertsantos/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ArtistsController;
+use App\Http\Controllers\BandsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 
 //except significa os campos que a gente quer que ele não tem
 Route::resource('artists', ArtistsController::class)->except(['create', 'edit']);
+Route::resource('bands', BandsController::class)->except(['create', 'edit']);
+
+// Caso queira mudar o nome padrão das funções como index, show...
+// Route::prefix('bandas')->group(function () {
+//   Route::get('/', [BandsController::class, 'listar']);
+//   Route::get('/{id}', [BandsController::class, 'exibir']);
+//   Route::post('/', [BandsController::class, 'cadastrar']);
+//   Route::patch('/{id}', [BandsController::class, 'atualizar']);
+//   Route::delete('/{id}', [BandsController::class, 'deletar']);
+// });

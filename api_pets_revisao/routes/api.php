5a8fce1c618aadd4451c92446362f45b4c1f4620
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-06 16:22:23 -0300

    Update das rotas

--- api_pets_revisao/routes/api.php
+++ api_pets_revisao/routes/api.php
@@ -1,19 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\RaceController;
+use App\Http\Controllers\SpecieController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+Route::get('races', [RaceController::class, 'index']);
+Route::post('races', [RaceController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('species', [SpecieController::class, 'index']);
+Route::post('species', [SpecieController::class, 'store']);
+
+Route::get('pets', [PetController::class, 'index']);
+Route::post('pets', [PetController::class, 'store']);

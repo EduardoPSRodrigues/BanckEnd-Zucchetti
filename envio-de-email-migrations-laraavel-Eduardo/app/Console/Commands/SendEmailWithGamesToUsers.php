13d8ae13d6b394eebcc889bd5a4fbe4977f7005f
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-07 15:11:49 -0300

    Create Commands para Send Email Wit hGames To Users

---
+++ envio-de-email-migrations-laraavel-Eduardo/app/Console/Commands/SendEmailWithGamesToUsers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendEmailWithGamesToUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-email-with-games-to-users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}

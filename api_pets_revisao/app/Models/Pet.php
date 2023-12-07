d4fec3080ffd940a2362798f55ae395d92f442a7
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-07 14:50:41 -0300

    Update Model Pet

--- api_pets_revisao/app/Models/Pet.php
+++ api_pets_revisao/app/Models/Pet.php
@@ -16,11 +16,17 @@ class Pet extends Model
 
     protected $hidden = ['created_at','updated_at']; //Esconde essas informações quando fizer um find ou all
 
+    /*Relacionamento
+    Estou em PET e quero fazer um relacionamento com as raças, sendo que
+    1 pet tem 1 raça
+    Regra do laravel se for um relacionamento que tem 1 classe, coloca no singular, se tiver mais relacionamentos
+    como hasmany, ai coloca no plural*/
     public function race() {
         return $this->hasOne(Race::class, 'id', 'race_id');
     }
 
     public function specie() {
+        //id é chave primeira da tabela specie e fará referencia com a coluna specie_id da tabela PET
         return $this->hasOne(Specie::class, 'id', 'specie_id');
     }
 }

609b7e312c0793a0e856e2e0d8cd52d1995913c1
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-07 14:52:43 -0300

    Update rotas da api

--- api_pets_revisao/routes/api.php
+++ api_pets_revisao/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PetController;
+use App\Http\Controllers\PetsReportController;
 use App\Http\Controllers\RaceController;
 use App\Http\Controllers\SpecieController;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +14,6 @@ Route::post('species', [SpecieController::class, 'store']);
 
 Route::get('pets', [PetController::class, 'index']);
 Route::post('pets', [PetController::class, 'store']);
+
+//Rota diferente do normal, sendo que é para exportar um documento pdf
+Route::get('pets/export', [PetsReportController::class, 'export']);

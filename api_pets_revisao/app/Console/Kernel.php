868fd72d1d3edf275b63606667774f15ef67daea
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-07 14:49:21 -0300

    Update Kernel

--- api_pets_revisao/app/Console/Kernel.php
+++ api_pets_revisao/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendServicePetEmail;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -10,9 +11,19 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
+
+    protected $commands = [
+        SendServicePetEmail::class
+    ];
+
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('app:send-service-pet-email')
+        ->timezone('America/Sao_Paulo')
+        ->everyMinute();
+
     }
 
     /**

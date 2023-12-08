3910bac5cb48420fc2fd01363acd03717e5aa18f
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-08 12:25:11 -0300

    Update

--- envio-de-email-migrations-laraavel-Eduardo/app/Console/Kernel.php
+++ envio-de-email-migrations-laraavel-Eduardo/app/Console/Kernel.php
@@ -2,17 +2,27 @@
 
 namespace App\Console;
 
+use App\Console\Commands\SendEmailWithGamesToUsers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+
+    protected $commands = [
+        SendEmailWithGamesToUsers::class
+    ];
+
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->command('app:send-email-with-games-to-users')
+        ->timezone('America/Sao_Paulo')
+        ->at('08:00');
     }
 
     /**

15f737c9ed56ba301300b05c70400a82a675bd48
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-12 16:24:47 -0300

    Create Model Client

---
+++ api_pets_revisao/app/Models/Client.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['people_id', 'bonus'];
+
+    //Relacionamento
+    public function people() {
+        return $this->hasOne(People::class, 'id', 'people_id');
+    }
+}

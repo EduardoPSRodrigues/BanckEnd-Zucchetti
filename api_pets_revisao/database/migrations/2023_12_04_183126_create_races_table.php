78764911553cc109f56ea7cd330df3bffd979404
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-12 16:25:32 -0300

    Update Migration Races

--- api_pets_revisao/database/migrations/2023_12_04_183126_create_races_table.php
+++ api_pets_revisao/database/migrations/2023_12_04_183126_create_races_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
         Schema::create('races', function (Blueprint $table) {
             $table->id();
             $table->string('name', 150);
+            //$table->softDeletes();
             $table->timestamps();
         });
     }

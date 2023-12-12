d5f4a0c94ec41bdeab38af384b9d78f004289379
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-12 16:27:21 -0300

    Update Routes Api

--- api_pets_revisao/routes/api.php
+++ api_pets_revisao/routes/api.php
@@ -1,19 +1,38 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\PetsReportController;
+use App\Http\Controllers\ProfessionalController;
 use App\Http\Controllers\RaceController;
 use App\Http\Controllers\SpecieController;
+use App\Http\Controllers\VaccineController;
 use Illuminate\Support\Facades\Route;
+
+// BODY -> cadastrar, atualizar
+// Query paraments -> GET -> listar dados
+// Route params -> DELETE, PUT, GET(unico)->middleware(['auth:sanctum']);
 
 Route::get('races', [RaceController::class, 'index']);
 Route::post('races', [RaceController::class, 'store']);
 
 Route::get('species', [SpecieController::class, 'index']);
 Route::post('species', [SpecieController::class, 'store']);
+Route::delete('species/{id}', [SpecieController::class, 'destroy']);
+//->middleware(['ability:delete-species']);
 
 Route::get('pets', [PetController::class, 'index']);
 Route::post('pets', [PetController::class, 'store']);
+Route::delete('pets/{id}', [PetController::class, 'destroy']);
+
 
 //Rota diferente do normal, sendo que é para exportar um documento pdf
 Route::get('pets/export', [PetsReportController::class, 'export']);
+
+Route::post('clients', [ClientController::class, 'store']); //->middleware(['ability:create-clients']);
+Route::get('clients', [ClientController::class, 'index']); //->middleware(['ability:get-clients']);
+
+Route::post('profissionals', [ProfessionalController::class, 'store']); //->middleware(['ability:create-profissionals']);
+Route::get('profissionals', [ProfessionalController::class, 'index']); //->middleware(['ability:get-profissionals']);
+
+Route::post('vaccines', [VaccineController::class, 'store']);//->middleware(['ability:create-vaccines']);

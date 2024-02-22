cc9aac6835141a651e6be80f8d75015140c18c8f
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:14:01 -0300

    Update AuthenticationService.js

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/services/AuthenticationService.js
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/services/AuthenticationService.js
@@ -6,6 +6,22 @@ class AuthenticationService {
         const response = await api.post('login', body)
         return response.data
     }
+
+    async createUser(body){
+        const response = await api.post('users', body)
+        return response.data
+    }
+
+    async getsUsers(){
+        const response = await api.get('users')
+        return response.data
+    }
+
+    
+    async logout(){
+        const response = await api.post('logout')
+        return response.data
+    }
 }
 
 export default new AuthenticationService();

7f987b1daded3c50a5d79a787cd08e5cf2c66bf5
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:13:08 -0300

    Update Home.spec.js

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/Home.spec.js
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/Home.spec.js
@@ -17,7 +17,6 @@ global.ResizeObserver = require('resize-observer-polyfill')
 describe("Tela Home", () => {
      
      vi.spyOn(SpecieService, 'getAllSpecies').mockResolvedValue([
-        //Não precisa considerar como esta cadastrado no backend e sim qual é o formato do array
         {
             id: 1,
             name: 'Cachorro'
@@ -68,6 +67,6 @@ describe("Tela Home", () => {
         
         const cards = component.findAll("[data-test='card-item']")
         
-        expect(cards).toHaveLength(3) //Contar se contou 3 cards
+        expect(cards).toHaveLength(3)
     })
 })

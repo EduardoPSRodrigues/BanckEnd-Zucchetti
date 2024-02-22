41a90281c9cf2af4691cadc114198a5e9fc82979
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:11:45 -0300

    Update Login.spec.js

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/Login.spec.js
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/Login.spec.js
@@ -22,7 +22,6 @@ describe('Tela de login', () => {
 
     it('Espera-se que a tela seja renderizada', () => {
         const component = mount(Login, {
-            //Habilitando o plugin do vuetify
             global: {
                 plugins: [vuetify]
             }
@@ -32,9 +31,6 @@ describe('Tela de login', () => {
 
     it('Espera-se que ao submeter o formulário, seja redirecionado para tela home', () => {
 
-        /*O teste não precisa da API para funcionar, como eu sei a resposta da API, então estou gerando
-        um dado de resposta da API e colocando no teste unitário. Pois quando clicar para fazer o login, 
-        será disparado a função AuthenticationService e preciso passar essas informações.*/
         const login = vi.spyOn(AuthenticationService, 'login').mockResolvedValue({
             data: {
                 token: 'token',
@@ -48,22 +44,20 @@ describe('Tela de login', () => {
             }
         })
 
-        /* GET quando for algo nativo do html
-        getComponent quando for um componente externo, exemplo vuetify
-        Não precisa ser o e-mail que esta no banco de dados, pode ser qualquer informação que o teste irá funcionar*/
+        // GET quando for algo nativo do html
+        // getComponente quando for um componente externo
         component.getComponent("[data-test='input-email']").setValue("h@gmail.com")
         component.getComponent("[data-test='input-password']").setValue("12345678")
         component.getComponent("[data-test='submit-button']").trigger("submit")
         
-        expect(login).toBeCalledTimes(1) //Testar se o método foi chamado apenas uma vez
-        expect(login).toBeCalledWith({email: 'h@gmail.com', password:  '12345678'}) //Ao clicar no botão submit eu espero que seja passado o email e a senha que esta preenchido no formulário
+        expect(login).toBeCalledTimes(1)
+        expect(login).toBeCalledWith({email: 'h@gmail.com', password:  '12345678'})
 
     })
     
-    //Para usar o await tem que usar o async
     it('Espera-se que ao submeter o formulário, receba uma mensagem de erro', async () => {
 
-        vi.spyOn(AuthenticationService, 'login').mockRejectedValue(new Error()) //Vai rejeitar o login e trazer uma mensagem de erro genérica
+        vi.spyOn(AuthenticationService, 'login').mockRejectedValue(new Error())
 
         const component = mount(Login, {
             global: {
@@ -77,7 +71,7 @@ describe('Tela de login', () => {
         component.getComponent("[data-test='input-password']").setValue("12345678")
         component.getComponent("[data-test='submit-button']").trigger("submit")
         
-        await flushPromises() //A msg demora para aparecer e para isso tem que usar esse codigo para aguardar a msg e verificar o seu valor, o teste é assincrono
+        await flushPromises()
         
         expect(component.text()).toContain("Houve um erro ao realizar o login")
     })

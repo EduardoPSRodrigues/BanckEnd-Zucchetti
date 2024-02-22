cac2ede6763347d7e54e7eccb3bb54162ac0f676
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:14:10 -0300

    Update index.js

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/router/index.js
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/router/index.js
@@ -5,7 +5,12 @@ import ListPets from '../views/ListPets.vue'
 import FormPet from '../views/FormPet.vue'
 import Login from '../views/Login.vue'
 import ListProfissionals from '../views/ListProfissionals.vue'
-import AprendizadoVue from '@/views/Aprendizado.vue'
+import FormUser from '../views/FormUser.vue'
+import VaccinationForm from '../views/VaccinationForm.vue'
+import Adoptions from '../views/Adoptions.vue'
+
+import AdoptionsDocuments from '../views/Documents.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +26,11 @@ const router = createRouter({
       component: Home
     },
     {
+      path: '/pets/editar/:id',
+      name: 'FormPetEdit',
+      component: FormPet
+    },
+    {
       path: '/pets/:id',
       name: 'ListPets',
       component: ListPets
@@ -29,16 +39,31 @@ const router = createRouter({
       path: '/pets/novo',
       name: 'FormPet',
       component: FormPet
-    },
+    }, 
     {
       path: '/veterinarios',
       name: 'ListProfissionals',
       component: ListProfissionals
     },
     {
-      path: '/aprendizado',
-      name: 'Aprendizado',
-      component: AprendizadoVue
+      path: '/usuarios',
+      name: 'FormUser',
+      component: FormUser
+    },
+    {
+      path: '/pets/:id/vacinacao',
+      name: 'VaccinationForm',
+      component: VaccinationForm
+    },
+    {
+      path: '/adocoes',
+      name: 'Adoptions',
+      component: Adoptions
+    },
+    {
+      path: '/adocoes/documentos/:id',
+      name: 'AdoptionsDocuments',
+      component: AdoptionsDocuments
     }
   ]
 })

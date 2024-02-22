72cc79fe4c66301f27f4f5fb5d8bcc23b81f9150
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:11:50 -0300

    Update Login.vue

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/Login.vue
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/Login.vue
@@ -5,16 +5,15 @@
       max-width="228"
       src="https://yt3.googleusercontent.com/N2jcFNzjfqRVZ07qkh3xn1VM7ka9Xa3O8o968DAOqoX4i1nRC_yv2hhcLfSf3tY5BJ6XcBuegg=s900-c-k-c0x00ffffff-no-rj"
     ></v-img>
-    
-    <v-card
-      class="mx-auto pa-12 pb-8"
-      elevation="8"
-      max-width="448"
-      rounded="lg"
-    >
-    
-      <v-alert v-if="showError" color="error" closable title="Houve um erro ao realizar o login"></v-alert>
-      
+
+    <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
+      <v-alert
+        v-if="showError"
+        color="error"
+        closable
+        title="Houve um erro ao realizar o login"
+      ></v-alert>
+
       <div class="text-subtitle-1 text-medium-emphasis mt-4">Account</div>
 
       <v-text-field
@@ -62,35 +61,36 @@ import api from '../services/api'
 
 import AuthenticationService from '../services/AuthenticationService'
 
-  export default {
-    data() {
-        return {
-            visible: false,
-            email: '',
-            password: '',
-            showError: false
-        }
-    },
-    methods: {
-        handleSubmit(){
-            AuthenticationService.login({
-                email: this.email,
-                password: this.password,
-            })
-            .then((data) => {   
-            
-            console.log("ENTREIIII AQUIII")
-            
-                api.defaults.headers.common['Authorization'] = `Bearer ${data.data.token}` 
-                localStorage.setItem("@token_petshop", data.data.token)
-                localStorage.setItem("@permissions_petshop", JSON.stringify(data.data.permissions))
-                this.$router.push("/home")
-            })
-            .catch((error) => {
-                console.log(error)
-               this.showError = true //Aparece a mensagem de erro na tela
-            })   
-        }
+export default {
+  data() {
+    return {
+      visible: false,
+      email: '',
+      password: '',
+      showError: false
+    }
+  },
+  methods: {
+    handleSubmit() {
+      AuthenticationService.login({
+        email: this.email,
+        password: this.password
+      })
+        .then((data) => {
+          console.log('ENTREIIII AQUIII')
+
+          api.defaults.headers.common['Authorization'] = `Bearer ${data.data.token}`
+          localStorage.setItem('@token_petshop', data.data.token) //Salvando informações no local storage
+          localStorage.setItem('@permissions_petshop', JSON.stringify(data.data.permissions))
+          localStorage.setItem('@name', data.data.name)
+          localStorage.setItem('@profile', data.data.profile)
+          this.$router.push('/home')
+        })
+        .catch((error) => {
+          console.log(error)
+          this.showError = true
+        })
     }
   }
+}
 </script>

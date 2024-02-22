902a147d2190b041b58c63c6112964a364dfa2d6
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:12:26 -0300

    Update ListPets.vue

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/ListPets.vue
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/ListPets.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container>
-    <h1>Lista de pets</h1>
+    <div class="d-flex justify-space-between">
+      <h1>Lista de pets</h1>
+      <v-btn color="orange" variant="flat" @click="handleExport"> Exportar dados </v-btn>
+    </div>
     <v-table>
       <thead class="header-table">
         <tr>
@@ -9,15 +12,39 @@
           <th class="text-left">Porte</th>
           <th class="text-left">Idade</th>
           <th class="text-left">Peso</th>
+          <td class="text-ledt">Ações</td>
         </tr>
       </thead>
       <tbody>
         <tr v-for="pet in pets" :key="pet.id">
           <td>{{ pet.pet_name }}</td>
           <td>{{ pet.race.name }}</td>
-           <td>{{ this.translateWeight(pet.size) }}</td>
-            <td>{{ pet.age }}</td>
-             <td>{{ pet.weight }}</td>
+          <td>{{ this.translateWeight(pet.size) }}</td>
+          <td>{{ pet.age }}</td>
+          <td>{{ pet.weight }}</td>
+          <td class="pa-4">
+            <v-menu>
+              <template v-slot:activator="{ props }">
+                <v-btn icon v-bind="props">
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="handleRedirectToEdit(pet.id)">
+                  <v-list-item-title>Editar</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="handleDeletePet(pet.id)">
+                  <v-list-item-title>Deletar</v-list-item-title>
+                </v-list-item>
+                <v-list-item @click="handleExportProfilePet(pet.id, pet.pet_name)">
+                  <v-list-item-title>Exportar</v-list-item-title>
+                </v-list-item>
+                 <v-list-item @click="handleRedirectVaccination(pet.id)">
+                  <v-list-item-title>Vacinação</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </td>
         </tr>
       </tbody>
     </v-table>
@@ -35,23 +62,51 @@ export default {
   },
   methods: {
     translateWeight(name) {
-       switch(name) {
+      switch (name) {
         case 'SMALL': {
           return 'PEQUENO'
         }
         case 'MEDIUM': {
           return 'MÉDIO'
         }
-        case 'LARGE' : {
+        case 'LARGE': {
           return 'GRANDE'
         }
-         case 'EXTRA_LARGE' : {
+        case 'EXTRA_LARGE': {
           return 'GIGANTE'
         }
         default: {
           return name
         }
-       }
+      }
+    },
+    handleDeletePet(petId) {
+      PetService.deleteOnePet(petId)
+        .then(() => {
+          alert('Deletado com sucesso')
+          this.pets = this.pets.filter((item) => item.id !== petId)
+        })
+        .catch(() => alert('Erro ao deletar'))
+    },
+    handleRedirectToEdit(petId) {
+      this.$router.push(`/pets/editar/${petId}`)
+    },
+    handleExport(){
+      PetService.export(this.$route.params.id)
+      .then(() => {
+        alert("Baixado com sucesso. verifique sua pasta de downloads")
+      })
+      .catch(() => alert("Houve ao exportar a lista de pets"))
+    },
+    handleExportProfilePet(petId, petName) {
+      PetService.exportProfilePet(petId, petName)
+       .then(() => {
+        alert("Baixado com sucesso. verifique sua pasta de downloads")
+      })
+      .catch(() => alert("Houve ao exportar a lista de pets"))
+    },
+    handleRedirectVaccination(petId) {
+      this.$router.push(`/pets/${petId}/vacinacao`)
     }
   },
   mounted() {
@@ -76,5 +131,4 @@ export default {
 tbody tr:nth-child(2n) {
   background: #f2f0f0;
 }
-
 </style>

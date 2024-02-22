e031a5c1c4f8f901086160c820bfedc0d24d313c
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:13:57 -0300

    Update PetService.js

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/services/PetService.js
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/services/PetService.js
@@ -1,4 +1,5 @@
 import api from "./api"
+import { saveAs } from "file-saver"
 
 class PetService {
     
@@ -11,6 +12,33 @@ class PetService {
         const response = await api.get(`pets?specie_id=${specie_id}`)
         return response.data
     }
+
+    async deleteOnePet(petId) {
+        const response = await api.delete(`pets/${petId}`)
+        return response.data
+    }
+
+    async findOnePet(petId) {
+        const response = await api.get(`pets/${petId}`)
+        return response.data
+    }
+
+    async editPet(body, petId) {
+        const response = await api.put(`pets/${petId}`, body)
+        return response.data
+    }
+
+    async export(specie_id){
+        const response = await api.get(`pets/export?specie_id=${specie_id}`, {responseType: 'blob'})
+        saveAs(response.data, 'lista_de_pets.pdf')
+    }
+
+    async exportProfilePet(petId, petName){
+        const response = await api.get(`pets/perfil?id=${petId}`, {responseType: 'blob'})
+        saveAs(response.data, `${petName}.pdf`)
+    }
+
+
 }
 
 export default new PetService();

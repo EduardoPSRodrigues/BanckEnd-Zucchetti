5fe60e962c936b61fbe55211b0cca375a2ce1f2b
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-02-22 16:12:42 -0300

    Update FormPet.vue

--- ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/FormPet.vue
+++ ProjetoPET-Front-e-Back/Pet-Frontend/gerenciamento-pets-frontend-eduardo/src/views/FormPet.vue
@@ -4,7 +4,11 @@
   </v-snackbar>
 
   <form @submit.prevent="handleSubmit">
-    <v-card width="80%" class="mx-auto px-6 mt-4" title="Cadastro de pet">
+    <v-card
+      width="80%"
+      class="mx-auto px-6 mt-4"
+      :title="petId ? 'Edição de pet' : 'Cadastro de pet'"
+    >
       <v-alert
         v-if="showError"
         color="error"
@@ -41,6 +45,7 @@
             v-model="weight"
             :error-messages="errors.weight"
             data-test="input-weight"
+            step="0.1"
           />
         </v-col>
       </v-row>
@@ -84,21 +89,9 @@
           />
         </v-col>
       </v-row>
-
-      <v-row>
-        <v-col cols="12" md="12">
-          <v-textarea
-            label="Descrição"
-            variant="outlined"
-            v-model="description"
-            data-test="input-description"
-          />
-        </v-col>
-      </v-row>
-
       <v-card-actions class="d-flex justify-end">
         <v-btn color="orange" type="submit" variant="flat" data-test="submit-button">
-          Cadastrar
+          {{ petId ? 'Editar' : 'Cadastrar' }}
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -132,10 +125,22 @@ export default {
       itemsRaces: [],
       success: false,
       errors: {},
-      showError: false
+      showError: false,
+      petId: this.$route.params.id
     }
   },
   mounted() {
+    if (this.petId) {
+      PetService.findOnePet(this.petId).then((data) => {
+        this.name = data.name
+        this.age = data.age
+        this.weight = data.weight
+        this.size = data.size
+        this.race_id = data.race_id
+        this.specie_id = data.specie_id
+      })
+    }
+
     SpecieService.getAllSpecies()
       .then((data) => {
         this.itemsSpecies = data
@@ -161,20 +166,28 @@ export default {
 
         schemaPetForm.validateSync(body, { abortEarly: false })
 
-        PetService.createPet(body)
-          .then(() => {
-            this.success = true
+        if (this.petId) {
+          PetService.editPet(body, this.petId)
+            .then(() => {
+              alert('Pet atualizado com sucesso')
+            })
+            .catch(() => alert('Houve um erro ao atualizar o pet'))
+        } else {
+          PetService.createPet(body)
+            .then(() => {
+              this.success = true
 
-            this.name = ''
-            this.age = 1
-            this.weight = 1
-            this.size = ''
-            this.specie_id = ''
-            this.race_id = ''
-          })
-          .catch(() => {
-            this.showError = true
-          })
+              this.name = ''
+              this.age = 1
+              this.weight = 1
+              this.size = ''
+              this.specie_id = ''
+              this.race_id = ''
+            })
+            .catch(() => {
+              this.showError = true
+            })
+        }
       } catch (error) {
         if (error instanceof yup.ValidationError) {
           this.errors = captureErrorYup(error)

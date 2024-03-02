100fc56871d5da04c9043bf1174a39f9e2dd50c9
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-03-02 10:28:16 -0300

    Update SolicitationDocument.php

--- ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/app/Models/SolicitationDocument.php
+++ ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/app/Models/SolicitationDocument.php
@@ -9,10 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class SolicitationDocument extends Model
 {
     use HasFactory;
-    use HasUuids;
+    use HasUuids; //Para gerar o uuid automatico
 
 
-    protected $table = "solicitations_documents";
+    protected $table = "solicitations_documents"; //para o código não ter dúvidas em relação ao nome da tabela
 
     protected $fillable = ['client_id', 'cpf', 'document_address', 'rg', 'term_adoption'];
 }

f92c97bcb4055d3c0503d24e6f71dc3b8a464a81
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-03-02 10:26:57 -0300

    Update TestCase.php

--- ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/tests/TestCase.php
+++ ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/tests/TestCase.php
@@ -9,13 +9,16 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase; //Permite que o banco seja limpo a cada teste
     use CreatesApplication;
 
     public function setUp(): void
     {
-        parent::setUp();
+        parent::setUp(); //chama essa classe para executar todo o codigo dela mais as linhas debaixo
+        //senao tivesse colocado esse comando, a classe setUp seria sobrescrevida
 
+        //Já que a seed é executada toda vez para criar o usuario inicial e o profiles, entao nao preciso
+        //chamar em todos os códigos, basta colocar no TestCase, pois esse codigo executa toda vez que o teste inicia
         $this->seed(Profiles::class);
         $this->seed(InitialUser::class);
     }

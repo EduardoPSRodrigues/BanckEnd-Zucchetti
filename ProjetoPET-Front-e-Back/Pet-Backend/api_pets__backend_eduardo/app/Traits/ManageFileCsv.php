2659cb10e65b16ce86ded0f6fe40b053a8b5925c
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-03-07 18:10:11 -0300

    Update ManageFileCsv.php

--- ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/app/Traits/ManageFileCsv.php
+++ ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/app/Traits/ManageFileCsv.php
@@ -7,17 +7,18 @@ trait ManageFileCsv
 
     public function getArrayCsv($file, int $amountData, string $separator = ';',)
     {
-        $contentFile = file_get_contents($file->getRealPath());
+        $contentFile = file_get_contents($file->getRealPath()); //pegar o arquivo e converter em uma string sendo que >getRealPath pega o caminho absoluto do arquivo
 
+        //amountData é a quantidade de coluna do arquivo
         // Converte o conteúdo CSV para uma matriz associativa
-
         // $csvData = array_map('str_getcsv', explode("\n", $contentFile)); -> importa com ,
-        // importa com ;
+
+        // importa com ; quando vai usar algo dentro do map precisa colocar o use
         $csvData = array_map(function ($row) use ($separator) {
             return str_getcsv($row, $separator);
         }, explode("\n", $contentFile));
 
-        // Pega as keys do array
+        // Pega as keys do array que é o cabeçalho, nome das tabelas
         $headers = array_shift($csvData);
 
         // remover caracteres estranhos das keys
@@ -27,7 +28,7 @@ trait ManageFileCsv
 
         foreach ($csvData as $row) {
             if (count($row) === $amountData) {
-                $csvArray[] = array_combine($headerParse, $row);
+                $csvArray[] = array_combine($headerParse, $row); //faz a combinação do titulo com as informações de forma individual
             }
         }
 

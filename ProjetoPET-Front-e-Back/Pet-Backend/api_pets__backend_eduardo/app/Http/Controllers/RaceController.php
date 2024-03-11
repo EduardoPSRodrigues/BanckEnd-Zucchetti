af1f6e5d32152d9f575767cfcc61164206287a1b
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2024-03-11 11:23:24 -0300

    Update RaceController.php

--- ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/app/Http/Controllers/RaceController.php
+++ ProjetoPET-Front-e-Back/Pet-Backend/api_pets__backend_eduardo/app/Http/Controllers/RaceController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+
 use App\Http\Requests\StoreRaceRequest;
-use App\Models\Race;
+
+use App\Http\Services\Race\CreateRaceService;
+use App\Http\Services\Race\GetAllRacesService;
 use App\Traits\HttpResponses;
 
-use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use Exception;
@@ -16,25 +18,43 @@ class RaceController extends Controller
 {
     use HttpResponses;
 
+    // public function index() {
+    //     $races = Race::all();
+    //     return $races;
+    // }
+
+    // public function store(StoreRaceRequest $request)
+    // {
+    //     try {
+
+    //         $body = $request->all();
+
+    //         $race = Race::create($body);
+
+    //         return $race;
+    //     } catch (Exception $exception) {
+    //         return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+    //     }
+    // }
+
     // Lista todos ou parcialmente os dados de um recurso
-    public function index() {
-        $races = Race::all();
+    public function index(GetAllRacesService $getAllRacesService)
+    {
+        $races = $getAllRacesService->handle();
         return $races;
     }
 
     //Colocar o nome da classe da validação para vincular a essa função
-    public function store(StoreRaceRequest $request)
+    public function store(StoreRaceRequest $request, CreateRaceService $createRaceService)
     {
         try {
-
             $body = $request->all();
-
-            $race = Race::create($body);
-
+            $race = $createRaceService->handle($body);
             return $race;
         } catch (Exception $exception) {
             return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
         }
     }
+
 
 }
